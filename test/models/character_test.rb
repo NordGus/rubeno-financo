28177b43a6e594f90d960a605cfd4d57c5dee310
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-09-13 15:16:49 +0200

    fixing test

--- test/models/character_test.rb
+++ test/models/character_test.rb
@@ -1,13 +1,4 @@
 require "test_helper"
 
 class CharacterTest < ActiveSupport::TestCase
-  test "#replace_padlock!" do
-    character = characters(:one)
-
-    previous_padlock_version = character.padlock_version
-
-    character.replace_padlock!
-
-    assert character.padlock_version != previous_padlock_version, "padlock_version did not changed"
-  end
 end
