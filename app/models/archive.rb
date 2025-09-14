502a183d96fb635fd803fe50f63eb79c0f5b1c8d
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-09-14 11:20:51 +0200

    adding useful scopes to archive

--- app/models/archive.rb
+++ app/models/archive.rb
@@ -1,4 +1,8 @@
 class Archive < ApplicationRecord
   belongs_to :owner, class_name: "Character", inverse_of: :archives
   has_many :sessions, inverse_of: :archive, dependent: :nullify, foreign_key: :archive_id
+
+  scope :accessible_by, ->(character_id) { where(character_id:) }
+  scope :editable_by, ->(character_id) { where(character_id:) }
+  scope :configurable_by, ->(character_id) { where(character_id:) }
 end
