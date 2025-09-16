9e2a4b420cb01fc60ce9f3764a436be6afff0d56
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-09-16 03:45:33 +0200

    unifying autentication logic for password_keys

--- app/models/password_key.rb
+++ app/models/password_key.rb
@@ -5,4 +5,8 @@ class PasswordKey < ApplicationRecord
 
   has_one :padlock, as: :keyable, dependent: :destroy
   has_one :character, through: :padlock
+
+  def self.authenticate(username: nil, password: nil)
+    authenticate_by(character: { email_address: username }, password:) || authenticate_by(character: { tag: username }, password:)
+  end
 end
