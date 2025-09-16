65be6e67368d4b85715942910ed262b7fac7d7cd
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-09-16 03:12:56 +0200

    adding default scope

--- app/models/password_key.rb
+++ app/models/password_key.rb
@@ -1,6 +1,8 @@
 class PasswordKey < ApplicationRecord
   has_secure_password
 
+  default_scope -> { includes(:character) }
+
   has_one :padlock, as: :keyable, dependent: :destroy
   has_one :character, through: :padlock
 end
