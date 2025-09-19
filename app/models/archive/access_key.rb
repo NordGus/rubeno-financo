0f607ed938e9d5c6eceb7e08fe56ae55c26c4050
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-09-19 22:40:18 +0200

    fixing active condition

--- app/models/archive/access_key.rb
+++ app/models/archive/access_key.rb
@@ -3,6 +3,6 @@ class Archive::AccessKey < ApplicationRecord
   belongs_to :archive
 
   scope :owned_by, ->(character_id) { where(owner_id: character_id) }
-  scope :active, -> { where(expires_at: [ ">= ?", Time.zone.now ]).or(where(expires_at: nil)) }
+  scope :active, -> { where("expires_at >= ?", Time.zone.now).or(where(expires_at: nil)) }
   scope :with_editable_access, -> { where.not(can_edit_since: nil) }
 end
