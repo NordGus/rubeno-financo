b0831b426ccc5f6578d359a1e756bdf42c07ace0
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-09-21 02:44:40 +0200

    adjusting routing configuration

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :passwords, param: :token, only: [ :new, :create, :edit, :update ]
 
   namespace :app do
-    resources :archives do
+    resources :archives, only: [ :index, :new, :create, :edit, :update, :destroy ] do
       post :access, on: :member
     end
   end
