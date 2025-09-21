61f3936861f6012c6ffdc5d79bd14c16ad668745
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-09-21 21:26:48 +0200

    configuring mission_control-jobs

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module Financo
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.mission_control.jobs.http_basic_auth_enabled = false
   end
 end
