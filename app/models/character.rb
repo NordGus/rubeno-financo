5c55df9626e96de0df55599448c0ec0137b1615f
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-10-05 11:56:26 +0200

    adding a job to asynchronously create the character's missing Archive::AccessKey after creation

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,4 +1,6 @@
 class Character < ApplicationRecord
+  after_create_commit :create_missing_access_keys_for_existing_archives_async
+
   validates :email_address, uniqueness: true, presence: true, email: true
   validates :tag, uniqueness: true, presence: true, length: { maximum: 256 }
 
@@ -10,4 +12,10 @@ class Character < ApplicationRecord
   has_many :sessions, dependent: :destroy
   has_many :archives, dependent: :destroy, foreign_key: :owner_id
   has_many :archive_access_keys, class_name: "Archive::AccessKey", dependent: :destroy, foreign_key: :owner_id
+
+  private
+
+  def create_missing_access_keys_for_existing_archives_async
+    Archive::AccessKey::CreateMissingKeysForCharacterJob.perform_later(self)
+  end
 end
---
+++ test/jobs/archive/access_key/create_missing_keys_for_character_job_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Archive::AccessKey::CreateMissingKeysForCharacterJobTest < ActiveJob::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
