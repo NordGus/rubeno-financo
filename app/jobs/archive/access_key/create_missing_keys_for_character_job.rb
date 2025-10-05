5c262e66c06749f8a1b0d1ce1fb968e4e1064ebc
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-10-05 23:22:15 +0200

    adding some more documentation comments

--- app/jobs/archive/access_key/create_missing_keys_for_character_job.rb
+++ app/jobs/archive/access_key/create_missing_keys_for_character_job.rb
@@ -6,7 +6,9 @@ class Archive::AccessKey::CreateMissingKeysForCharacterJob < ApplicationJob
 
     # NOTE: This is not a performant way to create the missing access keys, but it strongly enforces the validations.
     #   Also, financo is designed to work on a self-hosted deployment for their users. So the amount of users and/or
-    #   probably won't go pass the thousands in the current vision for the application.
+    #   probably won't go pass the thousands in the current vision for the application. Also, the concurrent traffic
+    #   into the instance will probably so low, that the probability of synchronization errors produced by this approach
+    #   will be also low.
     Archive.not.owned_by(character_id).not.accessible_by(character_id).find_each do |archive|
       archive.access_keys.create_with(can_view: false, can_edit: false).find_or_create_by!(character_id: character_id)
     end
