2fb407f05f96f01957230bb456061f56ad9abb78
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-10-20 00:25:44 +0200

    fixing Account::WithHistory concern

--- app/models/account/with_history.rb
+++ app/models/account/with_history.rb
@@ -4,7 +4,7 @@
 # Account::WithHistory is a concern to indicate that an account has a child history account to mark the initial balance
 # of the account.
 module Account::WithHistory
-  include ActiveSupport::Concern
+  extend ActiveSupport::Concern
 
   included do
     has_one :history, class_name: "Account::System::History", foreign_key: :parent_id, dependent: :destroy
