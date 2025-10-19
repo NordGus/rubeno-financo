e23b263852db97cad68e8b42a41015adcea03737
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-10-19 10:04:10 +0200

    adding documentation

--- app/models/account/system/history.rb
+++ app/models/account/system/history.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+##
+# Account::System::History is an account used to represent the balance of the parent account at a certain date, this is
+# one is used to include it on the archive's ledger(s).
 class Account::System::History < Account
   belongs_to :parent, class_name: "Account", foreign_key: :parent_id
 end
