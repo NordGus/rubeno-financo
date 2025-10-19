4c15eeb392f717f22832998036a3d2656d559048
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-10-20 01:06:41 +0200

    adding Account::Capital::Saving model

---
+++ app/models/account/capital/saving.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+##
+# Account::Capital::Saving represent a liquid savings account in the archive, mostly savings bank accounts.
+class Account::Capital::Saving < Account
+  include Account::WithHistory
+end
