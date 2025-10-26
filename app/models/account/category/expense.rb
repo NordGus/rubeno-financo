da3a74f9a5c31c32e1695bedb3fb4fec3f69141d
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-10-26 22:00:47 +0100

    adding Account::Category::Expense account type model

---
+++ app/models/account/category/expense.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+##
+# Account::Category::Expense represents an expense account or an family of expense accounts in the archive, like "Food",
+# "Transport", "Groceries", etc.
+class Account::Category::Expense < Account
+  include Account::IsCategory
+
+  has_many :children, class_name: "Account::Category::Expense", foreign_key: :parent_id, dependent: :destroy
+end
