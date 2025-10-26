6fd6f2f2d762e88ee7d8a8aa6a5f989b84172102
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-10-26 22:09:38 +0100

    adding Account::Category::Income account type model

---
+++ app/models/account/category/income.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+##
+# Account::Category::Income represents an income account or an family of income accounts in the archive, like "Salary",
+# "Bonus", "Interest", etc.
+class Account::Category::Income < Account
+  include Account::IsCategory
+
+  has_many :children, class_name: "Account::Category::Income", foreign_key: :parent_id, dependent: :destroy
+end
---
+++ test/models/account/category/income_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Account::Category::IncomeTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
