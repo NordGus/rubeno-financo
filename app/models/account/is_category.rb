78329b51170091538b9e31b1265633bc40add79a
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-10-26 21:51:11 +0100

    adding Account::IsCategory concern to handle categories

---
+++ app/models/account/is_category.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+##
+# Account::IsCategory is a concern to indicate that an account represents a category of expenses or incomes, that only
+# operates with the currency code "multi", these are mostly categorization accounts, like "Food", "Transport", "Groceries",.
+module Account::IsCategory
+  extend ActiveSupport::Concern
+
+  included do
+    enum :currency, %w[ multi ].index_by(&:itself), prefix: :operates_in
+
+    validate :child_has_the_same_currency_as_parent
+    validate :child_is_the_same_type_as_parent
+  end
+
+  class_methods do
+    private
+
+    def children_has_the_same_currency_as_parent
+      return unless parent.present?
+
+      errors.add(:currency, "must match parent currency") unless parent.currency == currency
+    end
+
+    def child_is_the_same_type_as_parent
+      return unless parent.present?
+
+      errors.add(:type, "must match parent type") unless parent.type == type
+    end
+  end
+end
