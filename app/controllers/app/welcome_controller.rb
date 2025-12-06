6824555b393a080ceabb76b13ceb520ca275b6f3
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-12-06 21:56:28 +0100

    fixing fixtures loading for file_system setup

--- app/controllers/app/welcome_controller.rb
+++ app/controllers/app/welcome_controller.rb
@@ -2,5 +2,10 @@
 
 class App::WelcomeController < AppController
   def index
+    @test_file = FileSystem::Item::File.find_by!(
+      name: "Monkey D Luffy.png",
+      parentable_type: "FileSystem::Item::Directory",
+      archive: Current.archive
+    )
   end
 end
