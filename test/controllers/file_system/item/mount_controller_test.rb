c266a53d2f2c1d16a421f77d880352403b903c6e
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-12-27 13:12:21 +0100

    making a shared file_system item partial to unify the file_system view

--- test/controllers/file_system/item/mount_controller_test.rb
+++ test/controllers/file_system/item/mount_controller_test.rb
@@ -1,8 +1,12 @@
 require "test_helper"
 
-class FileSystem::Item::MountControllerTest < ActionDispatch::IntegrationTest
+class FileSystem::Item::MountsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @mount = file_system_item_mounts(:protagonist_archive_checking_account_file_system)
+  end
+
   test "should get show" do
-    get file_system_item_mount_show_url
+    get file_system_item_mount_url(@mount)
     assert_response :success
   end
 end
