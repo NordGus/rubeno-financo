7787ed1529eddea947c7507d987c91ce31863527
Gustavo A. Rodriguez Rivero <gustavo.rodriguez92@gmail.com>
2025-12-29 02:40:53 +0100

    adding active storage package to app

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,4 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.esm.js"
 pin "@fortawesome/fontawesome-free", to: "@fortawesome--fontawesome-free--js--all.js" # @7.1.0
+pin "@rails/activestorage", to: "@rails--activestorage.js" # @8.1.100
